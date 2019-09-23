5e8724f0e70f0962c43272f03bfd73385878675d
theapache64 <theapache64@gmail.com>
2019-09-23 19:23:35 +0530

    🌟 add exercise model - moshi

--- app/src/main/java/com/theapache64/brogrammer/models/Excercise.kt
+++ app/src/main/java/com/theapache64/brogrammer/models/Excercise.kt
@@ -1,12 +1,32 @@
 package com.theapache64.brogrammer.models
 
-class Excercise(
-    val title: String,
-    val gifUrl: String,
-    val easyCount: Int,
-    val mediumCount: Int,
-    val hardCount: Int,
-    val easyDuration: Long,
-    val mediumDuration: Long,
-    val hardDuration: Long
-){}
+import com.squareup.moshi.Json
+
+data class Excercices(
+    @Json(name = "duration")
+    val duration: Duration,
+    @Json(name = "gif")
+    val gif: String, // https://raw.githubusercontent.com/sarthology/brogrammers/master/src/images/tricepdips.gif
+    @Json(name = "reps")
+    val reps: Reps,
+    @Json(name = "title")
+    val title: String // Triceps Dips
+) {
+    data class Reps(
+        @Json(name = "easy")
+        val easy: Int, // 10
+        @Json(name = "hard")
+        val hard: Int, // 20
+        @Json(name = "medium")
+        val medium: Int // 15
+    )
+
+    data class Duration(
+        @Json(name = "easy")
+        val easy: Int, // 0
+        @Json(name = "hard")
+        val hard: Int, // 0
+        @Json(name = "medium")
+        val medium: Int // 0
+    )
+}
